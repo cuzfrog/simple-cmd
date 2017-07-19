b66151962dea89394c946885cfbe95f59e176511
Cause Cheng <cuzfrog@139.com>
2017-07-19 18:14:52 +0800

    Bootstrap appDef

--- src/main/scala/com/github/cuzfrog/scmd/ProgramInfo.scala
+++ src/main/scala/com/github/cuzfrog/scmd/ProgramInfo.scala
@@ -1,7 +1,7 @@
 package com.github.cuzfrog.scmd
 
-private final case class ProgramInfo(name: String,
-                                     shortDescription: Option[String] = None,
-                                     version: Option[String] = None,
-                                     author: Option[String] = None)
+case class ProgramInfo(name: String,
+                       shortDescription: Option[String] = None,
+                       version: Option[String] = None,
+                       author: Option[String] = None)
 
--- src/main/scala/com/github/cuzfrog/scmd/package.scala
+++ src/main/scala/com/github/cuzfrog/scmd/package.scala
@@ -6,16 +6,26 @@ package object scmd {
 
   def paramDef[T](description: String = "",
                   isMandatory: Boolean = false,
-                  default: => T = Empty): Parameter[Nothing] = DummyParameter
+                  default: => T = Empty): Parameter[T] = DummyParameter
 
   def optDef[T](abbr: String = "",
                 description: String = "",
-                default: => T = Empty): OptionArg[Nothing] = DummyOptionArg
+                default: => T = Empty): OptionArg[T] = DummyOptionArg
+
+  def appDef(name: String,
+             shortDescription: Option[String] = None,
+             fullDescription: Option[String] = None,
+             version: Option[String] = None,
+             license: Option[String] = None,
+             author: Option[String] = None,
+             custom: Map[String, String] = Map.empty): ProgramInfo = DummyProgramInfo
+
 
   private object DummyCommand extends Command("", None)
   //private object DefaultEntry extends CommandEntry("", None, true)
   private object DummyParameter extends Parameter[Nothing]("")
   private object DummyOptionArg extends OptionArg[Nothing]("")
+  private object DummyProgramInfo extends ProgramInfo("")
 
   //private implicit def string2option(s: String): Option[String] = if (s == "") None else Option(s)
 
@@ -25,7 +35,6 @@ package object scmd {
     */
   private def Empty[T]: T =
     throw new IllegalArgumentException("Empty default value called. See scmd.Empty.")
-
 
   private[scmd] trait CanFormPrettyString[A] {
     def mkPrettyString(a: A): String

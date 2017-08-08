a8b01713283726c680b88774527559a861e8f25e
Cause Chung <cuzfrog@139.com>
2017-08-08 08:27:41 +0800

    Done todo:Scmd annotated class cannot be final.

--- src/main/scala/com/github/cuzfrog/scmd/macros/ScmdMacro.scala
+++ src/main/scala/com/github/cuzfrog/scmd/macros/ScmdMacro.scala
@@ -15,11 +15,10 @@ object MacroUtil {
   def apply(implName: scala.Symbol, defn: Tree): Stat = {
 
     val macroImpl: ScmdMacro = implName match {
-      case 'Def => new ScmdDefMacro
-      case 'Valid => new ScmdValidMacro
+      case 'Def => new ScmdDefMacro with ScmdMacroValidation
+      case 'Valid => new ScmdValidMacro with ScmdMacroValidation
       case bad => throw new AssertionError(s"No such ScmdMacro implementation:$bad")
     }
-    //todo: mods cannot contain final
     defn match {
       case cls: Defn.Class => macroImpl.expand(cls)
       case Term.Block(Seq(cls: Defn.Class, companion: Defn.Object)) =>
@@ -27,5 +26,14 @@ object MacroUtil {
       case _ =>
         abort(s"@${macroImpl.getClass.getSimpleName} must annotate a class.")
     }
+  }
+}
+
+private trait ScmdMacroValidation extends ScmdMacro {
+  abstract override def expand(cls: Defn.Class): Stat = {
+    val mods = cls.mods
+    require(!mods.map(_.structure).contains(mod"final".structure),
+      "Scmd Annotated class cannot be final.")
+    super.expand(cls)
   }
 }

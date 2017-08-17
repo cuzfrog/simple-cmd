9612af2a7abf2e6d3f879fc758ca436341784c3a
Cause Cheng <cuzfrog@139.com>
2017-08-17 17:14:44 +0800

    Add doc for DefStub

--- src/main/scala/Scmd/ScmdDefStub.scala
+++ src/main/scala/Scmd/ScmdDefStub.scala
@@ -6,10 +6,32 @@ import com.github.cuzfrog.scmd.{ArgRoute, PriorArg, ScmdDefApi}
 
 abstract class ScmdDefStub[D] extends ScmdDefApi {
   // ------------- stub methods -----------------
+
+  /**
+    * Add validation to this def-class.
+    *
+    * @param vali function that accepts def-class and return validation class.
+    * @tparam T validation class.
+    * @return the same def-class added with validation.
+    */
   def withValidation[T](vali: D => T): this.type
+  /**
+    * Provide route to define running of this def-class.
+    *
+    * @param toRoute function that accepts def-class and return [[ArgRoute]]
+    * @return true if the route has been run, false if route has been passed through.<br>
+    *         Exception: see [[runThrough]] in [[com.github.cuzfrog.scmd.RouteCommandOperations]]
+    */
   def runWithRoute(toRoute: D => ArgRoute): Boolean
+  /**
+    * Parse against arguments.
+    *
+    * @return evaluated def-class.
+    */
   def parse: D
   def defaultUsageString(implicit consoleType: ConsoleType): String
+  /** Built-in priorArg. */
   val help: PriorArg
+  /** Built-in priorArg. */
   val version: PriorArg
 }

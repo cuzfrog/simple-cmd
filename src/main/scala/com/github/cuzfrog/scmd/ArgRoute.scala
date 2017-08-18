badc8a4ff75a6cc44b6b6f4d68ee309d587207a7
Cause Cheng <cuzfrog@139.com>
2017-08-18 09:48:47 +0800

    Add prettyString log

--- src/main/scala/com/github/cuzfrog/scmd/ArgRoute.scala
+++ src/main/scala/com/github/cuzfrog/scmd/ArgRoute.scala
@@ -1,8 +1,11 @@
 package com.github.cuzfrog.scmd
 
+import com.github.cuzfrog.scmd.ScmdUtils.CanFormPrettyString
 import com.github.cuzfrog.scmd.internal.SimpleLogging
+import ScmdUtils._
 
 sealed trait ArgRoute extends SimpleLogging {
+  override protected implicit val loggerLevel: SimpleLogging.Level = SimpleLogging.Trace
   /** Trigger the route to run. */
   protected def execute: Boolean
 
@@ -13,8 +16,7 @@ private sealed case
 class CmdRoute private[scmd](cmd: Command,
                              conditions: Seq[RouteCondition] = Nil,
                              priorActions: Seq[(PriorArg, () => Unit)],
-                             next: Option[ArgRoute] = None)
-  extends ArgRoute {
+                             next: Option[ArgRoute] = None) extends ArgRoute {
   /**
     * Add inner func, not really run or execute.
     *
@@ -27,14 +29,14 @@ class CmdRoute private[scmd](cmd: Command,
   def run[R](innerF: => R, endRoute: Boolean)(implicit ev: R <:< ArgRoute = null): ArgRoute = {
     val _next: Option[ArgRoute] = Option(ev) match {
       case Some(_) => Option(innerF)
-      case None => Option(new RunRoute(() => innerF, endRoute))
+      case None => Option(RunRoute(() => innerF, endRoute))
     }
     this.copy(next = _next)
   }
 
   /** Trigger the run func. */
   override def execute: Boolean = this.cmd.met && {
-    debug(s"cmdRoute executed: $this | next: $next")
+    debug(s"cmdRoute executed: ${this.asInstanceOf[CmdRoute].prettyString}")
     val actions = priorActions collect {
       case (prior, action) if prior.met.contains(cmd.symbol) => action()
     }
@@ -53,17 +55,20 @@ class CmdRoute private[scmd](cmd: Command,
   }
 }
 
-private final class RunRoute private[scmd](runF: (() => R) forSome {type R},
-                                           endRoute: Boolean) extends ArgRoute {
+private final case class RunRoute private[scmd](runF: (() => R) forSome {type R},
+                                                endRoute: Boolean) extends ArgRoute {
   override def execute: Boolean = {
-    debug(s"runRoute executed: $this")
+    debug(s"runRoute executed: ${this.asInstanceOf[RunRoute].prettyString}")
     runF()
     endRoute
   }
   override def queryPrior(cmdSymbol: Symbol): Seq[PriorArg] = Nil
 }
 private final case class LinkRoute private[scmd](seq: Seq[ArgRoute]) extends ArgRoute {
-  override def execute: Boolean = seq.exists(_.execute)
+  override def execute: Boolean = {
+    debug(s"linkRoute executed: ${this.asInstanceOf[ArgRoute].prettyString}")
+    seq.exists(_.execute)
+  }
   override def queryPrior(cmdSymbol: Symbol): Seq[PriorArg] = {
     this.seq.flatMap(_.queryPrior(cmdSymbol))
   }
@@ -91,4 +96,29 @@ object ArgRoute {
     }
   }
 
+  private[scmd] implicit def canFormPrettyString: CanFormPrettyString[ArgRoute] = (a: ArgRoute) => {
+    recPrettyString(a)
+  }
+  private def recPrettyString(r: ArgRoute, indent: String = " "): String = r match {
+    case r: CmdRoute => indent + r.prettyString
+    case r: LinkRoute => "LinkRoute:" +
+      NEWLINE + r.seq.map(lr => recPrettyString(lr, indent + " ")).mkString(NEWLINE)
+    case r: RunRoute => indent + r.prettyString
+  }
+}
+
+private object CmdRoute {
+  implicit def canFormPrettyString: CanFormPrettyString[CmdRoute] = (a: CmdRoute) => {
+    val next = a.next match {
+      case None => None
+      case Some(r) => r.prettyString
+    }
+    s"CmdRoute(${a.cmd.name}->$next)"
+  }
+}
+
+private object RunRoute {
+  implicit def canFormPrettyString: CanFormPrettyString[RunRoute] = (a: RunRoute) => {
+    s"RunRoute(endRoute:${a.endRoute})"
+  }
 }

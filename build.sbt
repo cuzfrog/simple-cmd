5bf5ad45249356aa2aa61859bcc2eccadcd052a9
Cause Chung <cuzfrog@139.com>
2017-08-24 23:47:20 +0800

    Clean build and setup publishLocal

--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ import CommonSettings._
 shellPrompt in ThisBuild := { state => Project.extract(state).currentRef.project + "> " }
 //onLoad in Global := (onLoad in Global).value andThen (Command.process(s"", _))
 scalaVersion in ThisBuild := "2.12.3"
-val CompileOnly = config("compileonly").hide
+version in ThisBuild := "0.1.0-SNAPSHOT"
 
 val macroAnnotationSettings = Seq(
   addCompilerPlugin("org.scalameta" % "paradise" % "3.0.0-M10" cross CrossVersion.full),
@@ -21,15 +21,20 @@ val internalMacros = project
     ),
     publish := {},
     publishLocal := {}
-  ).configs(CompileOnly)
+  )
 
 val scmd = (project in file("."))
   .settings(
-    commonSettings, publicationSettings, readmeVersionSettings, macroAnnotationSettings
+    commonSettings, macroAnnotationSettings
   )
   .settings(
-    name := "simple-cmd"
-  ).dependsOn(internalMacros)
+    name := "scmd",
+    description := "simple-cmd, a command-line argument parsing library for scala.",
+    mappings in(Compile, packageBin) ++= mappings.in(internalMacros, Compile, packageBin).value,
+    mappings in(Compile, packageSrc) ++= mappings.in(internalMacros, Compile, packageSrc).value,
+    publishArtifact in (Compile, packageDoc) := false
+  ).dependsOn(internalMacros % "compile-internal, test-internal")
+
 
 val tests = project
   .settings(commonSettings, macroAnnotationSettings)
@@ -39,5 +44,5 @@ val tests = project
       "org.scalacheck" %% "scalacheck" % "1.13.4" % Test,
       "com.github.alexarchambault" %% "scalacheck-shapeless_1.13" % "1.1.6" % Test
     )
-  ).dependsOn(scmd % "compile->test;test->test")
+  ).dependsOn(scmd % "compile->test;test->test", internalMacros % "compile-internal, test-internal")
 
--- project/CommonSettings.scala
+++ project/CommonSettings.scala
@@ -3,20 +3,9 @@ import sbt._
 import MyTasks._
 
 object CommonSettings {
-
-  val loggingDependencies = Seq(
-    "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0",
-    "org.apache.logging.log4j" % "log4j-slf4j-impl" % "2.8.2",
-    "org.apache.logging.log4j" % "log4j-api" % "2.8.2",
-    "org.apache.logging.log4j" % "log4j-core" % "2.8.2"
-  )
-
   val commonSettings = Seq(
-    resolvers ++= Seq(
-      Resolver.bintrayRepo("cuzfrog", "maven")
-    ),
     organization := "com.github.cuzfrog",
-    crossScalaVersions := Seq("2.11.11", "2.12.2"),
+    crossScalaVersions := Seq("2.11.11", "2.12.3"),
     scalacOptions ++= Seq(
       //"-Xlint",
       "-unchecked",
@@ -30,24 +19,10 @@ object CommonSettings {
       "junit" % "junit" % "4.12" % "test",
       "com.novocode" % "junit-interface" % "0.11" % "test->default"
     ),
-    //libraryDependencies ++= loggingDependencies,
+    //autoAPIMappings := true,
     logBuffered in Test := false,
     testOptions += Tests.Argument(TestFrameworks.JUnit, "-v", "-q", "-a"),
     parallelExecution in Test := false,
     licenses += ("Apache-2.0", url("https://opensource.org/licenses/Apache-2.0"))
-  )
-
-  val publicationSettings = Seq(
-    publishTo := Some("My Bintray" at s"https://api.bintray.com/maven/cuzfrog/maven/${name.value}/;publish=1")
-  )
-
-  val readmeVersionSettings = Seq(
-    (compile in Compile) := ((compile in Compile) dependsOn versionReadme).value,
-    versionReadme := {
-      val contents = IO.read(file("README.md"))
-      val regex =raw"""(?<=libraryDependencies \+= "com\.github\.cuzfrog" %% "${name.value}" % ")[\d\w\-\.]+(?=")"""
-      val newContents = contents.replaceAll(regex, version.value)
-      IO.write(file("README.md"), newContents)
-    }
   )
 }
